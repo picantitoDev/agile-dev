1f717568fd10e621939f61e3de8452d7e828da10
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-04-11 12:12:22 -0500

    Initial Setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+app.use(express.static(path.join(__dirname, "public")))
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+  res.send("Gaaa")
+})
+
+app.listen(8080, () => {
+  console.log("Running on localhost...")
+})
