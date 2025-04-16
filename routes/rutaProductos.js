6ffd4e8b7a4f18930d8cdb7b77ac003653d3f3ca
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-04-15 22:10:21 -0500

    Redirect to New Form

--- routes/rutaProductos.js
+++ routes/rutaProductos.js
@@ -3,5 +3,8 @@ const router = express.Router()
 const controladorProductos = require("../controllers/controladorProductos")
 
 router.get("/", controladorProductos.obtenerProductos)
+router.get("/crear-producto", (req, res) => {
+  res.send("hola")
+})
 
 module.exports = router
